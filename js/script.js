a98d20eb7926bdb3dcae1b239771b6f68bb969ff
MikaelGrace <MikeOkoduwa700@gmail.com>
2025-02-04 05:56:27 +0100

    create variables for the operands and the operator -create a function that takes the operator and applies it to the operands

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+let firstOperand;
+let Operator;
+let secondOperand;
+
+function operate(Operator, num1, num2) {
+    switch (Operator) {
+        case "+" :
+            return num1 + num2;
+            break;
+        case "-" :
+            return num1 - num2;
+            break;
+        case "*" :
+            return num1 * num2;
+            break;
+        case "/" :
+            return num1 / num2;
+            break;
+        default :
+            return "invalid operator";
+    }
+}
+
+
+
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
